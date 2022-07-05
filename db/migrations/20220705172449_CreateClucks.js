0eba904aa815c56b5fcbbb4196d697f87f36eb96
Wendy Ng <yng@ucalgary.ca>
2022-07-05 19:15:07 +0000

    Part 5: The Clukcs Index and modify create clucks

--- db/migrations/20220705172449_CreateClucks.js
+++ db/migrations/20220705172449_CreateClucks.js
@@ -6,8 +6,8 @@ exports.up = function(knex) {
     return knex.schema.createTable('clucks',table=>{
         table.increments('id');
         table.string('username');
-        table.string('image_url');
-        table.string('content',1000);
+        table.text('image_url');
+        table.text('content');
         table.timestamp('createdAt').defaultTo(knex.fn.now());
         table.timestamp('updatedAt').defaultTo(knex.fn.now());
     })
--- routes/cluck.js
+++ routes/cluck.js
@@ -2,6 +2,12 @@ const express = require('express');
 const knex = require('../db/client');
 const router = express.Router()
 
+//Sign in page
+router.get('/home', (req, res) => {
+    let username = req.cookies.username;
+    res.render('home', {username: username}) 
+  })
+
 //Index page
 router.get('/', (req, res) => {
     knex('clucks')
@@ -18,6 +24,20 @@ router.get('/form', (req, res) => {
     res.render('form', {username: username}) 
   })
 
-  
+  //Create new cluck
+router.post('/form', (req, res) => {
+    let username = req.cookies.username;
+    console.log(req.body)
+    knex('clucks')
+    .insert({
+        username:username,
+        image_url:req.body.image_url,
+        content:req.body.content
+    })
+    .returning('*')
+    .then((clucks) => {
+        res.redirect('/')
+    })
+})
 
 module.exports = router;
